cc00b095e8065e6b2e78f0cf52e5f57001034c48
Falk Kühnel <falk@kuehnel.eu>
2025-05-22 17:23:42 +0200

    refactor: prepared multiple welcome messages

--- src/handlers/welcomeUsers/welcomeMessage.ts
+++ src/handlers/welcomeUsers/welcomeMessage.ts
@@ -1,17 +1,27 @@
-import {DiscordHandler, GuildMemberAddHandler} from "../../DiscordHandler";
+import {DiscordHandler, GuildMemberAddHandler} from "../../DiscordHandler/DiscordHandler";
 import {Client, Events, GuildMember} from "discord.js";
+
 
 export default class WelcomeMessage implements GuildMemberAddHandler{
     private channel: any;
+
+
     getName(): string {
         return "Welcome Message";
     }
-    eventType: Events.GuildMemberAdd = Events.GuildMemberAdd
+
+    eventType: Events = Events.GuildMemberAdd
     run(client: Client, handler: DiscordHandler, newMember: GuildMember) {
+        const welcomeMessages: string[] = [
+            "**Willkommen [user]**! Schön, dass Du da bist."
+
+        ];
+
         try {
             this.channel = newMember.guild.channels.cache.get(process.env.WELCOME_CHANNEL_ID)
+            const message = welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)].replace(/\[user]/g, `<@${newMember.id}>`);
             this.channel.send({
-                content: `**Willkommen <@${newMember.id}>**! Schön, dass Du da bist.`
+                content: message
             })
         } catch (e) {
             console.log(e)
