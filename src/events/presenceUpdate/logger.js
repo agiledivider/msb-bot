f5a20f8cab57e86cad5483b7453c308e466103a4
Falk Kühnel <falk@kuehnel.eu>
2025-06-05 16:31:51 +0200

    chore: deleted useless infos

--- src/events/presenceUpdate/logger.js
+++
@@ -1,4 +0,0 @@
-module.exports = (oldPresence, presence) => {
-  if (oldPresence?.status === presence?.status) return
-  console.log(`${presence.user.globalName} ist nun ${presence?.status} (${oldPresence?.status}) at ${(new Date()).toLocaleString('de-DE')}`);
-};
