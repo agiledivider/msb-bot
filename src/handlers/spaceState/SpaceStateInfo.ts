a458398bd84c3fba104f8b0b80bbb61ed633819c
Falk Kühnel <falk@kuehnel.eu>
2025-10-16 19:48:05 +0200

    feat: cleaning up old opening time messages

--- src/handlers/spaceState/SpaceStateInfo.ts
+++ src/handlers/spaceState/SpaceStateInfo.ts
@@ -1,130 +1,167 @@
-import {ClientReadyEvent} from "../../DiscordHandler/eventTypes";
-import {Client, Events, TextChannel} from "discord.js";
-import mqtt, {MqttClient} from "mqtt";
-import {Logger} from "pino";
-import {Config} from "../../DiscordHandler/DiscordHandler";
+import { ClientReadyEvent } from "../../DiscordHandler/eventTypes";
+import { Client, Events, TextChannel } from "discord.js";
+import mqtt, { MqttClient } from "mqtt";
+import { Logger } from "pino";
+import { Config } from "../../DiscordHandler/DiscordHandler";
 
 type State = {
-    open: boolean,
-    openUntil: string
-}
+  open: boolean;
+  openUntil: string;
+};
 
 class SpaceStateInfoHandler implements ClientReadyEvent {
-    eventType: Events.ClientReady = Events.ClientReady;
-    spaceStateInfo: SpaceStateInfoProcessor
+  eventType: Events.ClientReady = Events.ClientReady;
+  spaceStateInfo?: SpaceStateInfoProcessor;
 
-    execute([], {logger, client, config}) {
-        logger.debug('spaceStateInfo execute called')
-        logger.debug('spaceStateInfo %s', config)
-        if (!this.spaceStateInfo) {
-            //const guild = client.guilds.cache.get(config.spaceStateInfo.guildId);
-            logger.debug('spaceStateInfo channelId %s', config?.spaceStateInfo?.channelId)
-            //if (!guild) {
-            //    logger.error('guild not found')
-            const channel = client.channels.cache.get(config.spaceStateInfo.channelId);
-            if (!channel || !(channel instanceof TextChannel)) {
-                logger.error('channel not found %o', channel)
-                return
-            }
-            this.spaceStateInfo = new SpaceStateInfoProcessor(channel, logger, config)
-        }
+  execute([], { logger, client, config }) {
+    logger.debug("spaceStateInfo execute called");
+    logger.debug("spaceStateInfo %s", config);
+    if (!this.spaceStateInfo) {
+      //const guild = client.guilds.cache.get(config.spaceStateInfo.guildId);
+      logger.debug(
+        "spaceStateInfo channelId %s",
+        config?.spaceStateInfo?.channelId,
+      );
+      //if (!guild) {
+      //    logger.error('guild not found')
+      const channel = client.channels.cache.get(
+        config.spaceStateInfo.channelId,
+      );
+      if (!channel || !(channel instanceof TextChannel)) {
+        logger.error("channel not found %o", channel);
+        return;
+      }
+      this.spaceStateInfo = new SpaceStateInfoProcessor(
+        channel,
+        logger,
+        config,
+        client,
+      );
     }
+  }
 
-    getName(): string {
-        return "spacestate handler"
-    }
+  getName(): string {
+    return "spacestate handler";
+  }
 }
 
-export default SpaceStateInfoHandler
+export default SpaceStateInfoHandler;
 
 class SpaceStateInfoProcessor {
-    private mqttClient: MqttClient;
-    private channel: TextChannel
-    private lastState: State
-    private statechanges = []
-    private logger: Logger;
-    private config: any;
+  private mqttClient: MqttClient;
+  private channel: TextChannel;
+  private lastState: State;
+  private statechanges = [];
+  private logger: Logger;
+  private config: any;
+  private client: Client;
 
-    constructor(channel: TextChannel, logger: Logger, config) {
-        this.config = config
-        this.logger = logger.child({}, {msgPrefix: `[${this.constructor.name}] `})
-        this.channel = channel
-        this.mqttClient = mqtt.connect('mqtt://status.makerspacebonn.de', {
-            username: process.env.MQTT_USER,
-            password: process.env.MQTT_PASSWORD
-        })
+  constructor(channel: TextChannel, logger: Logger, config, client: Client) {
+    this.config = config;
+    this.logger = logger.child(
+      {},
+      { msgPrefix: `[${this.constructor.name}] ` },
+    );
+    this.channel = channel;
+    this.client = client;
+    const mqtturl = process.env.MQTT_URL || "mqtt://status.makerspacebonn.de";
+    this.logger.info("connecting MQTT URL: %s", mqtturl);
+    this.mqttClient = mqtt.connect(mqtturl, {
+      username: process.env.MQTT_USER,
+      password: process.env.MQTT_PASSWORD,
+    });
 
+    const mqttClient = this.mqttClient;
 
-        const mqttClient = this.mqttClient
+    mqttClient.on("message", async (topic, message) => {
+      this.logger.debug("message %o", message.toString());
+      const state = JSON.parse(message.toString()) as State;
+      if (this.lastState === undefined) {
+        this.lastState = state;
+        return;
+      }
 
+      this.logger.debug("state %o", state);
+      if (
+        this.lastState.open === state.open &&
+        this.lastState.openUntil === state.openUntil
+      )
+        return;
 
-        mqttClient.on('message', (topic, message) => {
-            this.logger.debug('message %o', message.toString())
-            const state = JSON.parse(message.toString()) as State
-            if (this.lastState === undefined) {
-                this.lastState = state
-                return
-            }
+      this.statechanges.push(state);
+      this.lastState = state;
+      const statesting = state.open ? "geöffnet." : "geschlossen.";
+      this.logger.debug("statestring %o", statesting);
+      if (state.open && state.openUntil) {
+        await this.channel.send({
+          content:
+            "Der Makerspace ist bis " + state.openUntil + " " + statesting,
+        });
+      } else {
+        await this.channel.send({
+          content: "Der Makerspace ist nun " + statesting,
+        });
+      }
 
-            this.logger.debug('state %o', state)
-            if (this.lastState.open === state.open && this.lastState.openUntil === state.openUntil) return
+      this.checkChangeAbuse();
+      this.cleanPreviousStateMessages();
+    });
 
-            this.statechanges.push(state)
-            this.lastState = state
-            const statesting = state.open ? 'geöffnet.' : 'geschlossen.'
-            this.logger.debug('statestring %o', statesting)
-            if (state.open && state.openUntil) {
-                this.channel.send({
-                    content: 'Der Makerspace ist bis ' + state.openUntil + ' ' + statesting
-                })
-            } else {
-                this.channel.send({
-                    content: 'Der Makerspace ist nun ' + statesting
-                })
-            }
+    mqttClient.on("connect", () => {
+      this.logger.info("connected to mqtt");
+      mqttClient.subscribe(this.config.spaceStateInfo.mqttTopic, () => {
+        this.logger.info("subscribed %o", this.config.spaceStateInfo.mqttTopic);
+      });
+    });
 
-            this.checkChangeAbuse()
-        })
+    mqttClient.on("error", (err) => {
+      this.logger.error("error", err);
+    });
 
-        mqttClient.on('connect', () => {
-            this.logger.info('connected to mqtt')
-            mqttClient.subscribe(this.config.spaceStateInfo.mqttTopic, () => {
-                this.logger.info('subscribed %o', this.config.spaceStateInfo.mqttTopic)
-            })
-        })
+    mqttClient.on("close", () => {
+      this.logger.warn("mqtt connection closed");
+    });
 
-        mqttClient.on('error', (err) => {
-            this.logger.error('error', err)
-        })
+    mqttClient.on("reconnect", () => {
+      this.logger.info("mqtt connection reconnected");
+    });
+  }
 
-        mqttClient.on('close', () => {
-            this.logger.warn('mqtt connection closed')
-        })
+  async cleanPreviousStateMessages() {
+    this.logger.debug("Cleaning previous state messages");
+    const messages = await this.channel.messages.fetch({ limit: 100 });
+    this.logger.debug(messages.at(0)?.createdTimestamp);
+    this.logger.debug(Date.now());
+    const stateMessages = messages.filter(
+      (msg) =>
+        msg.author.id === this.client.user.id &&
+        msg.content.includes("Der Makerspace ist") &&
+        msg.createdTimestamp < Date.now() - 5000,
+    );
 
-        mqttClient.on('reconnect', () => {
-            this.logger.info('mqtt connection reconnected')
-        })
-
+    this.logger.debug("Found %d state messages", stateMessages.size);
+    for (const [id, message] of stateMessages) {
+      try {
+        //this.logger.debug(message);
+        await message.delete();
+      } catch (error) {
+        console.warn(`Nachricht ${id} konnte nicht gelöscht werden:`, error);
+      }
     }
+  }
 
-
-    checkChangeAbuse () {
-        this.logger.debug('statechanges %o', this.statechanges)
-        if (this.statechanges.length > 4) {
-            this.statechanges = this.statechanges.filter(state => state.lastchange > (Date.now() / 1000 - 3600))
-            this.logger.debug('statechanges length %o', this.statechanges.length)
-            if (this.statechanges.length > 4) {
-                this.channel.send({
-                    content: 'Hör auf dauernd den Knopf zu drücken!'
-                })
-            }
-        }
+  checkChangeAbuse() {
+    this.logger.debug("statechanges %o", this.statechanges);
+    if (this.statechanges.length > 4) {
+      this.statechanges = this.statechanges.filter(
+        (state) => state.lastchange > Date.now() / 1000 - 3600,
+      );
+      this.logger.debug("statechanges length %o", this.statechanges.length);
+      if (this.statechanges.length > 4) {
+        this.channel.send({
+          content: "Hör auf dauernd den Knopf zu drücken!",
+        });
+      }
     }
+  }
 }
-
-
-
-
-
-
-
